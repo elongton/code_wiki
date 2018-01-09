061c93e0c1669ae83f673fed009f6d5edafe0a2c
Max Longton <elongton@Maxs-MacBook-Pro.local>
2018-01-09 12:42:20 -0500

    adding AWS

--- codewiki/settings/production.py
+++ codewiki/settings/production.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages',
+
     'django.contrib.sites',
     'django.contrib.humanize',
     'django_nyt',
