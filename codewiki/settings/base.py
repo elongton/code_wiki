fc7f89f85206643cb2b4110b077bd33b5614a928
Max Longton <elongton@Maxs-MacBook-Pro.local>
2018-01-09 16:50:05 -0500

    updated to house the storages package locally

--- codewiki/settings/base.py
+++ codewiki/settings/base.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages',
+
     'django.contrib.sites',
     'django.contrib.humanize',
     'django_nyt',
